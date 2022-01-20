9370b8b8b5527abc515365e888e3465679268064
Brennan Grout <bjg813@gmail.com>
2022-01-19 18:18:02 -0800

    turned slice into a variadic argument then built and ran program

--- src/github.com/Bjg813/average/main.go
+++ src/github.com/Bjg813/average/main.go
@@ -1,4 +1,4 @@
-// Average calculates the average of several numbers using a variadic function.
+// Average calculates the average of several numbers using a variadic function and a slice.
 package main
 
 import (
@@ -17,6 +17,8 @@ func average(numbers ...float64) float64 {
 }
 
 func main() {
+	// Arguments variable will hold the numbers entered from the terminal.
+	// Numbers slice will eventually hold the numbers we're averaging entered from the terminal.
 	arguments := os.Args[1:]
 	var numbers []float64
 	for _, argument := range arguments {
@@ -24,7 +26,13 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		// Append the converted number to the numbers slice.
 		numbers = append(numbers, number)
 	}
+	// Pass the slice to the variadic function and make turn the numbers slice into a variadic argument using (...) at end of slice numbers name.
 	fmt.Printf("Average: %0.2f\n", average(numbers...))
+	// Must use "go build main.go" in order to use this program in the terminal.
+	// Use ./main then write the numbers you want to average.
+	// Example: ./main 15 30 65
+	// 36.67
 }
